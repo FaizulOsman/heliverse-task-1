647637e5909afe3e8c8f33892b8bc80c691c32e9
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-01 14:17:23 +0600

    feat: redux setup done

---
+++ src/redux/features/user/userApi.js
@@ -0,0 +1,53 @@
+import api from "../../api/apiSlice";
+
+const userApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    insertData: builder.mutation({
+      query: (data) => ({
+        url: `/users`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["user"],
+    }),
+
+    getAllData: builder.query({
+      query: () => ({
+        url: `/users`,
+      }),
+      providesTags: ["user"],
+    }),
+
+    getSingleData: builder.query({
+      query: ({ id }) => ({
+        url: `/users/${id}`,
+      }),
+      providesTags: ["user"],
+    }),
+
+    deleteSingleData: builder.mutation({
+      query: ({ id }) => ({
+        url: `/users/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["user"],
+    }),
+
+    updateSingleData: builder.mutation({
+      query: ({ id, body }) => ({
+        url: `/users/${id}`,
+        method: "PUT",
+        body: body,
+      }),
+      invalidatesTags: ["user"],
+    }),
+  }),
+});
+
+export const {
+  useInsertDataMutation,
+  useGetAllDataQuery,
+  useGetSingleDataQuery,
+  useDeleteSingleDataMutation,
+  useUpdateSingleDataMutation,
+} = userApi;
