55640608f6d0d3da672c8075e55b06fc12d99ad3
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-01 10:26:14 +0600

    feat: initial setup done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Routers from "./routes";
+import "./styles/_global.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Routers />
+    </div>
+  );
+}
+
+export default App;
