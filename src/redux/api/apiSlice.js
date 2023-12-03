eacfa0e24ab9718b8c398313445e09185e6a3486
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-03 15:06:09 +0600

    feat: backend live site link added

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -4,7 +4,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    // baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://team-builder-server.vercel.app/api",
   }),
   tagTypes: ["user"],
   endpoints: () => ({}),
