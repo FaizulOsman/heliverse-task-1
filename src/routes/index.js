aeb0e75fd1fae764c7b365c8b53df772d9e8fa11
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-04 23:04:14 +0600

    update: route  updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,18 +11,6 @@ import MainLayout from "../layouts/MainLayout";
 const Routers = () => {
   return (
     <BrowserRouter>
-      {/* <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/users" element={<Users />} />
-        <Route exact path="/teams" element={<Team />} />
-        <Route exact path="/users/:id" element={<SingleUser />} />
-        <Route
-          exact
-          path="/users/create-new-user"
-          element={<CreateNewUser />}
-        />
-        <Route exact path="/users/create-team" element={<CreateTeam />} />
-      </Routes> */}
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
