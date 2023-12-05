c21110017cd328a5433bccb7591877c43195a839
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-05 10:51:02 +0600

    update: favicon and title changed + team card updated

--- src/components/UI/TeamCard.jsx
+++ src/components/UI/TeamCard.jsx
@@ -27,7 +27,7 @@ const TeamCard = ({ data, showViewTeamButton }) => {
 
   return (
     <div>
-      <div className="max-w-x relative">
+      <div className="max-w-x relative h-full">
         <div className="absolute right-5 top-5">
           <Modal
             Button={
@@ -74,50 +74,51 @@ const TeamCard = ({ data, showViewTeamButton }) => {
             }
           />
         </div>
-        <div className="bg-[#1d1836] shadow-xl rounded-lg py-3">
-          <h4 className="text-xl text-gray-400 font-bold text-center p-2">
-            {data?.team_name}
-          </h4>
-          <h4 className="text-lg text-green-400 font-bold p-5">User List</h4>
+        <div className="flex flex-col justify-between bg-[#1d1836] shadow-xl rounded-lg py-3 h-full">
+          <div>
+            <h4 className="text-xl text-gray-400 font-bold text-center p-2">
+              {data?.team_name}
+            </h4>
+            <h4 className="text-lg text-green-400 font-bold p-5">User List</h4>
 
-          <div className="p-2">
-            <table className="w-full text-left">
-              <thead>
-                <tr className="font-bold text-blue-500">
-                  <th key="name" className="sm:px-3 pt-0 pb-3">
-                    Name
-                  </th>
-                  <th key="domain" className="sm:px-3 pt-0 pb-3">
-                    Domain
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {data?.users?.map((user, index) => (
-                  <tr
-                    key={index}
-                    className="border-b border-gray-800 text-gray-400 text-sm"
-                  >
-                    <td className="sm:p-3 py-2">
-                      {user?.first_name} {user.last_name}
-                    </td>
-                    <td className="sm:p-3 py-2">{user?.domain}</td>
+            <div className="p-2">
+              <table className="w-full text-left">
+                <thead>
+                  <tr className="font-bold text-blue-500">
+                    <th key="name" className="sm:px-3 pt-0 pb-3">
+                      Name
+                    </th>
+                    <th key="domain" className="sm:px-3 pt-0 pb-3">
+                      Domain
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-
-            {showViewTeamButton && (
-              <div className="text-center my-3">
-                <Link
-                  className="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium"
-                  to={`/teams/${data?.id}`}
-                >
-                  Team Details
-                </Link>
-              </div>
-            )}
+                </thead>
+                <tbody>
+                  {data?.users?.map((user, index) => (
+                    <tr
+                      key={index}
+                      className="border-b border-gray-800 text-gray-400 text-sm"
+                    >
+                      <td className="sm:p-3 py-2">
+                        {user?.first_name} {user.last_name}
+                      </td>
+                      <td className="sm:p-3 py-2">{user?.domain}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
+          {showViewTeamButton && (
+            <div className="text-center my-3">
+              <Link
+                className="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium"
+                to={`/teams/${data?.id}`}
+              >
+                Team Details
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
